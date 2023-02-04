78df286feaa97b447c488cccd8d6e0320d699e15
arjun <arjunshajiforpc@gmail.com>
2023-02-04 19:28:55 +0530

    setup admin root,view products,add products

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
+
   let products = [
     {
       name : "Iphone 11",
@@ -26,7 +27,17 @@ router.get('/', function(req, res, next) {
       image : "https://www.apple.com/v/iphone-14-pro/c/images/meta/iphone-14-pro_overview__e414c54gtu6a_og.png?202302030110"
     }
   ]
-  res.render('index', { products,admin:false });
+
+  res.render('admin/view-products',{products,admin : true})
 });
+
+router.get('/add-products',(req,res)=>{
+  res.render('admin/add-products')
+})
+
+router.post('/add-products',(req,res)=>{
+  console.log(req.body);
+  console.log(req.files.productImage);
+})
 
 module.exports = router;
