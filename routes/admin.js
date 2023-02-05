2317b09eb819d03ade7625bae795db7823876cd9
arjun <arjunshajiforpc@gmail.com>
2023-02-05 10:31:37 +0530

    product displayed successfull on admin panel and user home form db

--- routes/admin.js
+++ routes/admin.js
@@ -5,32 +5,10 @@ var productHelper = require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-
-  let products = [
-    {
-      name: "Iphone 11",
-      category: "mobile",
-      description: "This is a good phone",
-      image: "https://www.apple.com/v/iphone-14-pro/c/images/meta/iphone-14-pro_overview__e414c54gtu6a_og.png?202302030110"
-    }, {
-      name: "Iphone 12",
-      category: "mobile",
-      description: "This is a good phone",
-      image: "https://www.apple.com/v/iphone-14-pro/c/images/meta/iphone-14-pro_overview__e414c54gtu6a_og.png?202302030110"
-    }, {
-      name: "Iphone 13",
-      category: "mobile",
-      description: "This is a good phone",
-      image: "https://www.apple.com/v/iphone-14-pro/c/images/meta/iphone-14-pro_overview__e414c54gtu6a_og.png?202302030110"
-    }, {
-      name: "Iphone 14",
-      category: "mobile",
-      description: "This is a good phone",
-      image: "https://www.apple.com/v/iphone-14-pro/c/images/meta/iphone-14-pro_overview__e414c54gtu6a_og.png?202302030110"
-    }
-  ]
-
-  res.render('admin/view-products', { products, admin: true })
+  productHelper.getAllProducts().then((products)=>{
+    // console.log(products);
+    res.render('admin/view-products', {products, admin: true })
+  })
 });
 
 router.get('/add-products', (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var productHelper = require('../helpers/product-helpers')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let products = [
-    {
-      name : "Iphone 11",
-      category : "mobile",
-      description : "This is a good phone",
-      image : "https://www.apple.com/v/iphone-14-pro/c/images/meta/iphone-14-pro_overview__e414c54gtu6a_og.png?202302030110"
-    },{
-      name : "Iphone 12",
-      category : "mobile",
-      description : "This is a good phone",
-      image : "https://www.apple.com/v/iphone-14-pro/c/images/meta/iphone-14-pro_overview__e414c54gtu6a_og.png?202302030110"
-    },{
-      name : "Iphone 13",
-      category : "mobile",
-      description : "This is a good phone",
-      image : "https://www.apple.com/v/iphone-14-pro/c/images/meta/iphone-14-pro_overview__e414c54gtu6a_og.png?202302030110"
-    },{
-      name : "Iphone 14",
-      category : "mobile",
-      description : "This is a good phone",
-      image : "https://www.apple.com/v/iphone-14-pro/c/images/meta/iphone-14-pro_overview__e414c54gtu6a_og.png?202302030110"
-    }
-  ]
-  res.render('index', { products,admin:false });
+router.get('/', function (req, res, next) {
+  productHelper.getAllProducts().then((products) => {
+    // console.log(products);
+    res.render('user/view-products', { products, admin: false })
+  })
 });
 
 module.exports = router;
